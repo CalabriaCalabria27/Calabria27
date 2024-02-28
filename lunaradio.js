da079b7037ddc4d57da5a20c79b9bf831ca517e3
CalabriaCalabria27 <154616259+CalabriaCalabria27@users.noreply.github.com>
2024-02-28 15:45:58 +0100

    Update lunaradio.js

--- lunaradio.js
+++ lunaradio.js
@@ -474,7 +474,7 @@ y("", "")
 
 
 function zeno() {
-zurl = "https://zenoplay.zenomedia.com/api/zenofm/nowplaying/dzsuinmlh3mtv/?rand=0.8855782183372827"+ Math.random();
+url = "https://zenoplay.zenomedia.com/api/zenofm/nowplaying/dzsuinmlh3mtv/?rand="+ Math.random();
 if (sa === "fallback") {url = "fallback.php?url="+zurl} else {url = zurl}
 
 d.ajax({
