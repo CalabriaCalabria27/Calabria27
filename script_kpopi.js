a2c541c5b1e414934b7ccb55d47d61d0eb2f28bd
CalabriaCalabria27 <154616259+CalabriaCalabria27@users.noreply.github.com>
2024-08-13 01:57:47 +0200

    Update script_kpopi.js

--- script_kpopi.js
+++ script_kpopi.js
@@ -5,8 +5,9 @@ const URL_STREAMING = 'https://stream-152.zeno.fm/dzsuinmlh3mtv?zt=eyJhbGciOiJIU
 
 var TYPE_API ='SPOTIFY'; //selecciona entre itunes , deezer o spotify xd
 
+
 //API URL /
-const API_URL = 'https://api.zeno.fm/mounts/metadata/subscribe/dzsuinmlh3mtv'+URL_STREAMING
+const API_URL = 'https://apikpop.conexionkpop.com/api_v4.php?url='+URL_STREAMING
 
 // Visit https://api.vagalume.com.br/docs/ to get your API key
 const API_KEY = "18fe07917957c289983464588aabddfb";
