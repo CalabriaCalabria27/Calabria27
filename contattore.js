7734740e4d97f7e8d7db0a29d71066792b295659
CalabriaCalabria27 <154616259+CalabriaCalabria27@users.noreply.github.com>
2024-11-13 21:51:43 +0100

    Add files via upload

---
+++ contattore.js
@@ -0,0 +1,6 @@
+const visitData = {
+    visitCount: visitCount
+};
+
+// Potresti inviare visitData a un server qui, usando fetch o XMLHttpRequest
+console.log(JSON.stringify(visitData)); // Mostra il JSON nel console log per prova
